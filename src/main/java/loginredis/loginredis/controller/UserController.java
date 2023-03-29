74387db6261ae76295a14a0566b51c3ec85f06ef
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-03-30 00:28:31 +0900

    chore: 클래스 네이밍 수정

--- src/main/java/loginredis/loginredis/controller/UserController.java
+++ src/main/java/loginredis/loginredis/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class SessionController {
+public class UserController {
 
 
     private final UserService userService;
